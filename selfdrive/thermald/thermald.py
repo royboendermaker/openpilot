b4c7b4f33e0012cbb8a71fb2d01bdb6764873b51
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2020-08-31 19:52:14 -0400

    testing

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -229,7 +229,7 @@ def thermald_thread():
 
     current_filter.update(msg.thermal.batteryCurrent / 1e6)
 
-    # TODO: add car battery voltage check
+    # TODO: add car battery voltage check test
     max_cpu_temp = max(msg.thermal.cpu0, msg.thermal.cpu1,
                        msg.thermal.cpu2, msg.thermal.cpu3) / 10.0
     max_comp_temp = max(max_cpu_temp, msg.thermal.mem / 10., msg.thermal.gpu / 10.)
