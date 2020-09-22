64070a8f77bdd49b4cdbe79036ddf958924fd683
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2020-09-22 11:04:38 -0400

    testing pqTimebombBypass

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -169,7 +169,8 @@ class CarInterface(CarInterfaceBase):
       if ret.cruiseState.enabled:
         self.pqCounter += 1
       if self.pqCounter >= 330: #time in seconds until counter threshold for pqTimebombWarn alert
-        events.append(create_event('pqTimebombWarn', [ET.WARNING]))
+        if not self.wheelGrabbed:
+          events.append(create_event('pqTimebombWarn', [ET.WARNING]))
         if self.wheelGrabbed or ret.steeringPressed:
           self.wheelGrabbed = True
           ret.stopSteering = True
