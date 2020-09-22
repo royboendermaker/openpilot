fdf84b1ab4d6e5e17eac20d664b2cd3a9ffd8c0e
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2020-09-22 14:35:41 -0400

    added print command for debug of stopSteering

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -73,6 +73,7 @@ class CarController():
         
         if CS.out.stopSteering:
           apply_steer = 0
+          print("CarController can see stopSteering being activated. Steering should be zero while this message is here.")
 
         # FAULT AVOIDANCE: HCA must not be enabled for >360 seconds. Sending
         # a single frame with HCA disabled is an effective workaround.
