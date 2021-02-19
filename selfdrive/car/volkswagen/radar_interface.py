4757b5ef8ab234946564d90486eb333ea9c2097b
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-02-19 18:33:38 -0500

    testing

--- selfdrive/car/volkswagen/radar_interface.py
+++ selfdrive/car/volkswagen/radar_interface.py
@@ -1,28 +1,58 @@
 #!/usr/bin/env python3
 from opendbc.can.parser import CANParser
 from cereal import car
-from selfdrive.car.toyota.values import NO_DSU_CAR, DBC, TSS2_CAR
 from selfdrive.car.interfaces import RadarInterfaceBase
 
 def _create_radar_can_parser(car_fingerprint):
-  if car_fingerprint in TSS2_CAR:
-    RADAR_A_MSGS = list(range(0x180, 0x190))
-    RADAR_B_MSGS = list(range(0x190, 0x1a0))
-  else:
-    RADAR_A_MSGS = list(range(0x310, 0x36F))
-    RADAR_B_MSGS = list(range(0x311, 0x36F))
+  RADAR_A_MSGS = list(range(0x310, 0x36F))
+  RADAR_B_MSGS = list(range(0x311, 0x36F))
 
   msg_a_n = len(RADAR_A_MSGS)
   msg_b_n = len(RADAR_B_MSGS)
 
-  signals = list(zip(['LONG_DIST'] * msg_a_n + ['NEW_TRACK'] * msg_a_n + ['LAT_DIST'] * msg_a_n +
-                ['REL_SPEED'] * msg_a_n + ['VALID'] * msg_a_n + ['SCORE'] * msg_b_n,
-                RADAR_A_MSGS * 5 + RADAR_B_MSGS,
-                [255] * msg_a_n + [1] * msg_a_n + [0] * msg_a_n + [0] * msg_a_n + [0] * msg_a_n + [0] * msg_b_n))
+  signals = list(
+        zip(
+            ["LongDist"] * msg_a_n
+            + ["LatDist"] * msg_a_n
+            + ["LongSpeed"] * msg_a_n
+            + ["LongAccel"] * msg_a_n
+            + ["Valid"] * msg_a_n
+            + ["Tracked"] * msg_a_n
+            + ["Meas"] * msg_a_n
+            + ["ProbExist"] * msg_a_n
+            + ["Index"] * msg_a_n
+            + ["ProbObstacle"] * msg_a_n
+            + ["LatSpeed"] * msg_b_n
+            + ["Index2"] * msg_b_n
+            + ["Class"] * msg_b_n
+            + ["ProbClass"] * msg_b_n
+            + ["Length"] * msg_b_n
+            + ["dZ"] * msg_b_n
+            + ["MovingState"] * msg_b_n,
+            RADAR_A_MSGS * 10 + RADAR_B_MSGS * 7,
+            [255.0] * msg_a_n
+            + [0.0] * msg_a_n
+            + [0.0] * msg_a_n
+            + [0.0] * msg_a_n
+            + [0] * msg_a_n
+            + [0] * msg_a_n
+            + [0] * msg_a_n
+            + [0.0] * msg_a_n
+            + [0] * msg_a_n
+            + [0.0] * msg_a_n
+            + [0.0] * msg_b_n
+            + [0] * msg_b_n
+            + [0] * msg_b_n
+            + [0.0] * msg_b_n
+            + [0.0] * msg_b_n
+            + [0.0] * msg_b_n
+            + [0] * msg_b_n,
+        )
+    )
 
-  checks = list(zip(RADAR_A_MSGS + RADAR_B_MSGS, [20]*(msg_a_n + msg_b_n)))
+  checks = list(zip(RADAR_A_MSGS + RADAR_B_MSGS, [6] * (msg_a_n + msg_b_n)))
 
-  return CANParser(DBC[car_fingerprint]['radar'], signals, checks, 1)
+  return CANParser(os.path.splitext(dbc_f)[0].encode("utf8"), signals, checks, 2)
 
 class RadarInterface(RadarInterfaceBase):
   def __init__(self, CP):
@@ -30,12 +60,8 @@ class RadarInterface(RadarInterfaceBase):
     self.track_id = 0
     self.radar_ts = CP.radarTimeStep
 
-    if CP.carFingerprint in TSS2_CAR:
-      self.RADAR_A_MSGS = list(range(0x180, 0x190))
-      self.RADAR_B_MSGS = list(range(0x190, 0x1a0))
-    else:
-      self.RADAR_A_MSGS = list(range(0x310, 0x36F))
-      self.RADAR_B_MSGS = list(range(0x311, 0x36F))
+    self.RADAR_A_MSGS = list(range(0x310, 0x36F))
+    self.RADAR_B_MSGS = list(range(0x311, 0x36F))
 
     self.valid_cnt = {key: 0 for key in self.RADAR_A_MSGS}
 
@@ -48,7 +74,7 @@ class RadarInterface(RadarInterfaceBase):
     self.no_radar = CP.carFingerprint in NO_DSU_CAR and CP.carFingerprint not in TSS2_CAR
 
   def update(self, can_strings):
-    if self.no_radar:
+    if self.radar_off_can:
       return super().update(None)
 
     vls = self.rcp.update_strings(can_strings)
