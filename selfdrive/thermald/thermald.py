39871d61e33ca26dd2477daa9a5a0270f761ec5c
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-02-24 18:02:11 -0500

    fixing?

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -224,7 +224,7 @@ def thermald_thread():
       # Setup fan handler on first connect to panda
       if handle_fan is None and health.health.hwType != log.HealthData.HwType.unknown:
         is_uno = health.health.hwType == log.HealthData.HwType.uno
-        has_relay = health.health.hwType in [log.PandaState.PandaType.blackPanda, log.PandaState.PandaType.uno, log.PandaState.PandaType.dos]
+        has_relay = health.health.hwType in [log.HealthData.HwType.blackPanda, log.HealthData.HwType.uno, log.HealthData.HwType.dos]
 
         if (not EON) or is_uno:
           cloudlog.info("Setting up UNO fan handler")
