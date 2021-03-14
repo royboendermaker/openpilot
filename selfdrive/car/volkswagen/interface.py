0821015b005badd7d48a8e38b948e4d6e57190a6
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2021-03-13 23:33:02 -0500

    testing CS.hcaSwitch as boolian

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -182,7 +182,7 @@ class CarInterface(CarInterfaceBase):
 
     #PQTIMEBOMB STUFF START
     #Warning alert for the 6min timebomb found on PQ's
-    ret.hcaSwitch = 7
+    ret.hcaSwitch = False
     ret.stopSteering = False
     if True: #(self.frame % 100) == 0: # Set this to false/False if you want to turn this feature OFF!
       if ret.cruiseState.enabled:
@@ -197,7 +197,7 @@ class CarInterface(CarInterfaceBase):
         if self.wheelGrabbed or ret.steeringPressed or True:
           self.wheelGrabbed = True
           # ret.stopSteering = True
-          ret.hcaSwitch = 5
+          ret.hcaSwitch = True
           self.pqBypassCounter += 1
           if self.pqBypassCounter >= 1.05*100: #time alloted for bypass
             self.wheelGrabbed = False
